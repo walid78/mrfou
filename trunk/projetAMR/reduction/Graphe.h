bd077bffd2e0a2d4d6a1b718556444d2fe31f9cc
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-10-16 09:11:40 +0000

    .h

--- trunk/projetAMR/reduction/Graphe.h
+++ trunk/projetAMR/reduction/Graphe.h
@@ -1,18 +1,18 @@
+#include <Vector>
+
+using namespace std;
 
 class Graphe {
 
 public:
-	Graphe();
+	Graphe(int nb_nodes);
 	~Graphe();
 	int addVertex();
 	void addEdges(int, int);
 	int getNeighbour(int, int);
 	Vector<int> getNeighbours(int);
-	Vector<int> getEdges(int);
-	Vector<int[2]> getEdges();
-	Vector<int> getVertex();
-	
+
 private:
-	Vector<int> vertex;
-	Vector<int[2]> edges;
+	Vector<int>[] graph;
+	int nb_nodes
 };
