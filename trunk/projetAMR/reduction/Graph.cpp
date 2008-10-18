ad93a24776b3a89c48196949625577f95958d809
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-10-18 11:19:24 +0000

    Utilisation de la première case du vector<int>[i] pour tout sommet i pour stocker le nombre de sommets j tels que i->j dans le graphe.

--- trunk/projetAMR/reduction/Graph.cpp
+++ trunk/projetAMR/reduction/Graph.cpp
@@ -6,7 +6,9 @@
 //===========================================================================
 Graph::Graph(const int nodes){
 	for(int i=0 ; i<nodes ; ++i)
-	  graph.push_back(vector<int>());
+		//Initialisation d'une liste pour chaque sommet i où la première case 
+		//contient le nombre de sommets en relation avec i dans le graphe
+	  graph.push_back(vector<int>(1,0));
 }
 
 /** Destructeur : **/
@@ -19,13 +21,15 @@ Graph::~Graph(){
 //===========================================================================
 void Graph::addEdges(const int originNode, const int destNode)
 {
-	for(unsigned i=0 ; i<graph[originNode].size() ; ++i){
+	for(unsigned i=1 ; i<graph[originNode].size() ; ++i){
 		if(graph[originNode][i] == destNode)
 			return ;
 	}
 	
 	graph[originNode].push_back(destNode);
+	graph[originNode][0]++;
 	graph[destNode].push_back(originNode);
+	graph[destNode][0]++;
 }
 
 //===========================================================================
@@ -42,10 +46,12 @@ vector<int>& Graph::operator[](const int originNode){
   return graph[originNode];
 }
 */
+
+//===========================================================================
 ostream& operator<<(ostream& o, const Graph& g){
 	for(unsigned i=0 ; i<g.graph.size() ; ++i){
-		o << i << ": ";
-		for(unsigned j=0 ; j<g.graph[i].size() ; ++j){
+		o << i << ":";
+		for(unsigned j=1 ; j<g.graph[i].size() ; ++j){
 			o << " " << g.graph[i][j];
 		}
 		o << endl;
@@ -53,5 +59,3 @@ ostream& operator<<(ostream& o, const Graph& g){
 	
 	return o;
 }
-
-
