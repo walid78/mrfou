cb5dcb3e26ba413e2ede801efd9cb6b355b5e4ad
killerpapy <killerpapy@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-10-18 12:31:40 +0000

    Dans Solve: readGraph: Passage au switch case et non if de if de if readGraph et getNbVertex: Traitement du dernier element de ligne lu!

--- projetAMR/reduction/Solve.cpp
+++ projetAMR/reduction/Solve.cpp
@@ -16,29 +16,33 @@ int getNbVertex(char* path){
     while(getline(file, line)){  // tant que l'on peut mettre la ligne dans "line"
       length = line.length();
       for(int i = 0; i<length ; i++){
-				switch (line[i]){
-				case '-':
-					current = atoi(number.c_str());
-					if(current > max)
-						max = current;
-					number = "";
-					break;
-				case ' ':
-					current = atoi(number.c_str());
-					if(current > max)
-						max = current;
-					number = "";
-					break;
-				default:
-					number += line[i];
-					break;
-				}
+	switch (line[i]){
+	case '-':
+	  current = atoi(number.c_str());
+	  if(current > max)
+	    max = current;
+	  number = "";
+	  break;
+	case ' ':
+	  current = atoi(number.c_str());
+	  if(current > max)
+	    max = current;
+	  number = "";
+	  break;
+	default:
+	  number += line[i];
+	}
       }
+      //Ici on traite le cas du dernier numero de la ligne!
+      current = atoi(number.c_str());
+      if(current > max)
+	max = current;
+      number = "";
     }
   }
   else  // sinon
     cerr << "Impossible d'ouvrir le fichier !" << endl;
-	
+  
   file.close();  // on ferme le fichier
   return max+1;
 }
@@ -48,48 +52,55 @@ readGraph(char* path){
   int nbVertex = getNbVertex(path);
   Graph graph(nbVertex);
   ifstream file(path, ios::in);  // on ouvre le fichier en lecture
-
+  
   if(file){  // si l'ouverture a reussi
     string line;
-		
+    
     while(getline(file, line)){  // tant que l'on peut mettre la ligne dans "line"
       int length = line.length();
       string number = "";
       int current, last=-1;
-			
+      
       for(int i = 0; i < length; i++){
-				if(line[i] == '-'){
-					current = atoi(number.c_str());
-					if(last >= 0){
-						graph.addEdges(last,current);
-					}
-					last = current;
-					number = "";
-				}
-				else if(line[i] == ' '){
-					current = atoi(number.c_str());
-					graph.addEdges(last,current);
-					number = "";
-					last = -1;
-				}
-				else{
-					number += line[i];
-				}
+	switch(line[i]){
+	case '-':
+	  current = atoi(number.c_str());
+	  if(last >= 0)
+	    graph.addEdges(last,current);
+       	  last = current;
+	  number = "";
+	  break;
+	case ' ':
+	  current = atoi(number.c_str());
+	  graph.addEdges(last,current);
+	  number = "";
+	  last = -1;
+	  break;
+	default:
+	  number += line[i];
+	}
       }
+      //Ici on traite le cas du dernier numero de la ligne!
+      current = atoi(number.c_str());
+      graph.addEdges(last,current);
+      number = "";
+      last = -1;
     }
   }
   else  // sinon
-		cerr << "Impossible d'ouvrir le fichier !" << endl;
-	
+    cerr << "Impossible d'ouvrir le fichier !" << endl;
+  
   file.close();  // on ferme le fichier
   return graph;
 }
 
 int
 main(int argc, char** argv){
+
+  cout << "test a :" << getNbVertex(argv[1])<<endl;
   Graph graph = readGraph(argv[1]);
   cout << "test reussi" << endl;
-	cout << graph << endl;
+  cout << graph << endl;
 	
   return 0;
 }
