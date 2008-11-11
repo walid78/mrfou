bd8f7f9b74709166ca07694fd3dc31c7c08edf74
killerpapy <killerpapy@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-11-11 15:47:20 +0000

    Changement des ref sur annexe et ajout d'un commentaire

--- trunk/projetAMR/reduction/Solve.cpp
+++ trunk/projetAMR/reduction/Solve.cpp
@@ -98,7 +98,7 @@ readGraph(string path){
 	  break;
 	case ' ':
 	  current = atoi(number.c_str());
-	  if(last != -1)
+	  if(last != -1)//si on ne rentre pas dans le if le sommet n' pas d'aretes.
 	    graph.addEdges(last,current);
 	  number = "";
 	  last = -1;
