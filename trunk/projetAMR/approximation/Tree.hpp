950cbaa53db525383a118394c0f2e03c01e3ae09
benoit.vedrenne@gmail.com <benoit.vedrenne@gmail.com@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-02 16:55:45 +0000

    Quelques ajouts

--- trunk/projetAMR/approximation/Tree.hpp
+++ trunk/projetAMR/approximation/Tree.hpp
@@ -11,8 +11,21 @@ public:
 
   Graph getGraph(void);
 
+  //Sous quelle forme doit être la couverture ???
+
+  //PARTIE UN
+  //Couverture à calculer
+
+  //PARTIE DEUX
+  Tree getTree();  //Il nous faut récupérer un arbre sans feuille
+  //Calcule de couverture
+  vector<int> vertexCover1();
+  //Regarder le cours pour le couplage maximal
+  vector<int> vertexCover2();
+
 private:
   Graph graph; //delegate
 }
 
 #endif //TREE_HPP
+ 
