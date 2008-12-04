5fe3a0c2ba7ead7dd6fbfdb62e194db6771c512f
benoit.vedrenne <benoit.vedrenne@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-04 12:51:15 +0000

    Impementation de quelques algorithmes enfin queelques, c'est vite dit.

--- projetAMR/approximation/Tree.cpp
+++ projetAMR/approximation/Tree.cpp
@@ -10,30 +10,55 @@ Graph Tree::getGraph(void){
 
 //Calculer un arbre sans feuille (surement virer ça)
 Tree Tree::getTree(){
+  vector<int> vertexCover;
+  int cpt = 0;
   int length = graph.getNbVertexes();
   for(int i = 0; i < length; ++i) {
-    if(graph.getNbNeighbours() == 1 /*&& racine == i*/) {
-      //Retirer le sommet i de la couverture
+    if(!(graph.getNbNeighbours(i) == 1 && i != 0)) { //Zero est toujours
+						  //la racine
+      vertexCover[cpt++] = i;
     }
   }
 }
 
 
 vector<int> Tree::vertexCover1() {
-  vector<int> vector();
+  vector<int> vertexCover();
   int last = 0;
   int length = graph.getNbVertexes();
   
-  for(int i = 0; i < length; ++i) {
-    if(graph.getNbNeighbours() == 1 /*&& racine == i*/) { //vérifier qu'on ne prends pas la racine !!!!
-      vector[last++] = i;
+  for(int i = 1; i < length; ++i) {
+    if((graph.getNbNeighbours(i) != 1) || (i == 0)) { //Zero est
+						    //toujours la racine
+      vertexCover[last++] = i;
     }
   }
   
-  return vector;
+  return vertexCover;
 }
 
-//Algo du cours à regarder (oui, je l'ai préter
+//Algo du cours à regarder (oui, je l'ai préter)
 vector<int> Tree::vertexCover2() {
+  //Chercher une feuille v
+  int leaf = findLeaf();
 
+  //Retenir dans la couverture le sommet u voisin du sommet v
+  vector<int> neighboors = graph.getNeighbours(leaf);
+  int vertex = neighboors[0];
+  index = 0;
+  vector<int> vertexCover[index++] = vertex;
+
+  //Eliminer toutes les aretes incidentes � u
+  //FAUT TROUVER LE TRUC MAGIQUE
+
+  //Travailler avec la foret restante
+  //FAIRE L'APPEL RECURSIF
+}
+
+int Tree::findLeaf() { //V�rifier que les sommets sont toujours couvrable
+  int length = graph.getNbVertexes();
+  for(int i = 1; i < length; ++i) {
+    if(graph.getNeighbours(i) == 1)
+      return i;
+  }
 }
