ce4060471606f7a2c0dec9da7323fc3c6ccb66f0
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-07 13:40:04 +0000

    deplacement 2

---
+++ trunk/projetAMR/approximation/old/Tree.hpp
@@ -0,0 +1,37 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+#include <vector>
+#include <string>
+
+#include "Graph.hpp"
+
+class Tree{
+
+public:
+  /*  Constructeur  */
+  Tree(int node);
+
+  Graph getGraph(void);
+
+  //Sous quelle forme doit être la couverture ???
+
+  //PARTIE UN
+  //Couverture à calculer : c'est vertexCover2
+
+  //PARTIE DEUX
+  Tree getTree();  //Il nous faut récupérer un arbre sans feuille
+  //Calcule de couverture
+  vector<int> vertexCover1();
+  //Regarder le cours pour le couplage maximal
+  vector<int> vertexCover2();
+  vector<int> vertexCover3();
+
+private:
+  Graph graph; //delegate
+
+  int findLeaf();
+}
+
+#endif //TREE_HPP
+ 
