4d5897e6c2314ffd6f58b6905d98ad525f0e1b4e
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-07 14:09:36 +0000

    maj pourri

--- trunk/projetAMR/approximation/Tree.hpp
+++ trunk/projetAMR/approximation/Tree.hpp
@@ -1,6 +1,31 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
+#include "Graph.hpp"
 
+using namespace std;
+
+class Tree{
+  
+public:
+  //===========================================================================
+  /** Constructeur : **/
+  Tree(const Graph& g, int node); 
+
+  //===========================================================================
+  /** Constructeur : **/
+  ~Tree();
+
+  //===========================================================================
+  /** Fonctions membres : **/
+  bool isLeaf();
+  
+private:
+  //==========================================================================
+  /** Attributs **/
+  Graph graph;
+  int nbVertexes;
+  
+}
 
 #endif
