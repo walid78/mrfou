250e09d6bd8a4305ab46c53b669832a3b5afc4f1
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-07 15:22:55 +0000

    ajout des &

--- trunk/projetAMR/approximation/Tree.cpp
+++ trunk/projetAMR/approximation/Tree.cpp
@@ -4,13 +4,14 @@
 
 /** Constructeur : **/
 //===========================================================================
-Tree::Tree(Graph g, int node = 0):nbVertexes(g.getNbVertexes()){
+Tree::Tree(Graph& g, int node = 0):nbVertexes(g.getNbVertexes()){
   root = node;
   makeTreeDFS(g, node);
 }
 
 /** Fonctions membres : **/
 //===========================================================================
-void Tree::makeTreeDFS(Graph g, int node){
-  tree=Graph(nbVertexes, "");
+void Tree::makeTreeDFS(Graph& g, int node){
+  tree = Graph(nbVertexes, "");
 }
+
