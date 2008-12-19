f6f5707d8d23e2ee6f0e57e32d31f5397de2ab6d
bbassac <bbassac@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-19 10:47:12 +0000

    mise a jour creation + calcul occupation

--- projetBDA/creation_tables.sql
+++ projetBDA/creation_tables.sql
@@ -29,55 +29,58 @@ drop tablespace ts7 including contents and datafiles;
 drop tablespace ts8 including contents and datafiles;
 drop tablespace ts9 including contents and datafiles;
 drop tablespace ts10 including contents and datafiles;
+drop tablespace ts11 including contents and datafiles;
 
 --CREATION TABLESPACES
 
 create tablespace ts0
-datafile 'ts0.dbf' size 10M
+datafile 'ts0.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts1
-datafile 'ts1.dbf' size 10M
+datafile 'ts1.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts2
-datafile 'ts2.dbf' size 10M
+datafile 'ts2.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts3
-datafile 'ts3.dbf' size 10M
+datafile 'ts3.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts4
-datafile 'ts4.dbf' size 10M
+datafile 'ts4.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts5
-datafile 'ts5.dbf' size 10M
+datafile 'ts5.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts6
-datafile' ts6.dbf' size 10M
+datafile' ts6.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts7
-datafile 'ts7.dbf' size 10M
+datafile 'ts7.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts8
-datafile 'ts8.dbf' size 10M
+datafile 'ts8.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 create tablespace ts9
-datafile 'ts9.dbf' size 10M
+datafile 'ts9.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 
 create tablespace ts10
-datafile 'ts10.dbf' size 10M
+datafile 'ts10.dbf' size 20M
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
-
+create tablespace ts11
+datafile 'ts11.dbf' size 20M
+EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 
 
@@ -91,74 +94,107 @@ EXTENT MANAGEMENT LOCAL AUTOALLOCATE ;
 
 --CREATION DES TABLES
 
-create table Destination(	ID_Dest number primary key,Nom_Destination varchar2(20) not null ,
-				Pays varchar2(20) not null) tablespace ts0;
+create table Destination(
+	ID_Dest number primary key,
+	Nom_Destination varchar2(20) not null ,
+	Pays varchar2(20) not null) tablespace ts0;
 
 
-create table Circuit(	ID_Circuit number primary key,
-			Nom_Circuit varchar2(20)not null)tablespace ts0 ;
+create table Circuit(	
+	ID_Circuit number primary key,
+	Nom_Circuit varchar2(20)not null)tablespace ts0 ;
 
-create table Assoc_Dest_Circuit(ID_Dest number, ID_Circuit number)tablespace ts0 ;
+create table Assoc_Dest_Circuit(
+	ID_Dest number, 
+	ID_Circuit number)tablespace ts0 ;
 
 --passer les 2 clés en en une clé composée. 
-create table Etape(	ID_Etape number ,
-			ID_Circuit number ,
-			Descriptif varchar(50) default ' ')  tablespace ts0  ;
+create table Etape(	
+	ID_Etape number ,
+	ID_Circuit number ,
+	Descriptif varchar(50) default ' ')  tablespace ts0  ;
 
-create table Reservation ( ID_Client number not null, 
-			   ID_Hotel number not null,
-			   Date_reservation date) tablespace ts0;
+create table Reservation ( 
+	ID_Client number not null, 
+	ID_Hotel number not null,
+	Date_reservation date) tablespace ts0;
 
-create table Hotel(	ID_Hotel number primary key, ID_Etape number not null,
-			Nom_Hotel varchar2(20) not null,Adresse varchar2(50) default('adresse inconnue') ,
-			ID_Classe number default(1), Capac_S number default(0) CHECK (Capac_S >0), 
-			Capac_D number default(0) CHECK (Capac_D >0)  )tablespace ts0 ;		
+create table Hotel(	
+	ID_Hotel number primary key, 
+	ID_Etape number not null,
+	Nom_Hotel varchar2(20) not null,
+	Adresse varchar2(50) default('adresse inconnue') ,
+	ID_Classe number default(1),
+	Capac_S number default(0) CHECK (Capac_S >0), 
+	Capac_D number default(0) CHECK (Capac_D >0)  )tablespace ts0 ;		
 
-create table Classe_Hotel(	ID_Classe number primary key, Prix_S float not null CHECK (Prix_S >0.0) , 
-				Prix_D float not null CHECK (Prix_D >0.0) )tablespace ts0 ;
+create table Classe_Hotel(		
+	ID_Classe number primary key, 
+	Prix_S float not null CHECK (Prix_S >0.0) , 
+	Prix_D float not null CHECK (Prix_D >0.0) )tablespace ts0 ;
 
 --create table Assoc_Hotel_Circuit(ID_Hotel number, ID_Circuit number)tablespace ts0 ;
 
-create table Sejour(	ID_Sejour number primary key, Duree number not null CHECK (Duree >0) , 
-			Description varchar2(50) default(' '), 
-			Coeff float default (1.0) CHECK (Coeff >0.0) )tablespace ts0 ;					
+create table Sejour(	
+	ID_Sejour number primary key, 
+	Duree number not null CHECK (Duree >0) , 
+	Description varchar2(50) default(' '), 
+	Coeff float default (1.0) CHECK (Coeff >0.0) )tablespace ts0 ;					
 
-create table Assoc_Prix_Sejour_Circuit(	ID_Circuit number, 
-					ID_Sejour number, 
-					Prix float not null  CHECK (Prix >0.0)  )tablespace ts0 ;
+create table Assoc_Prix_Sejour_Circuit(	
+	ID_Circuit number, 
+	ID_Sejour number, 
+	Prix float not null  CHECK (Prix >0.0)  )tablespace ts0 ;
 
 --Prix aller/retour
-create table Vol(	ID_Vol number primary key, ID_Dest number, 
-			Prix_Enfant float CHECK (Prix_Enfant >0.0) , 
-			Prix_Adulte float CHECK (Prix_Adulte >0.0) )tablespace ts0 ;
+create table Vol(	
+	ID_Vol number primary key, ID_Dest number, 
+	Prix_Enfant float CHECK (Prix_Enfant >0.0) , 
+	Prix_Adulte float CHECK (Prix_Adulte >0.0) )tablespace ts0 ;
 
-create table Client(	ID_Client number primary key ,Adresse varchar2(50),Tel varchar2(10), 
-			Nom varchar2(20) not null,Prenom varchar2(20)not null, 
-       	     	    	Age number   CHECK (Age >0 and Age < 150) , Email varchar2(30),
-			Classe_Sociale varchar2(20), ID_Dest_Preferee number, 
-		    	Investissement_Moyen float CHECK (Investissement_Moyen > 0.0))tablespace ts0 ;
+create table Client(	
+	ID_Client number primary key ,
+	Adresse varchar2(50),Tel varchar2(10), 
+	Nom varchar2(20) not null,Prenom varchar2(20)not null, 
+	Age number   CHECK (Age >0 and Age < 150) ,
+	Email varchar2(30),
+	Classe_Sociale varchar2(20),
+	ID_Dest_Preferee number,
+	Investissement_Moyen float CHECK (Investissement_Moyen > 0.0))tablespace ts0 ;
 --un seul hotel pour le client
 --rajouter le nombre passager enfant et adulte
 
-create table Facturation(ID_Facture number not null, Date_Facture date default trunc(sysdate), 
-       	     		Adresse_Client varchar2(50) not null, Tel varchar2(10) not null, 
-			Nom varchar2(20) not null, Prenom varchar2(20) not null, 
-			Nom_Dest varchar2(20)not null, Pays_Dest varchar2(20) not null, 
-			Nom_Hotel varchar2(20)not null, Address_Hotel varchar2(50) not null,
+create table Facturation(ID_Facture number not null,
+			Date_Facture date default trunc(sysdate), 
+       	     		Adresse_Client varchar2(50) not null, 
+			Tel varchar2(10) not null, 
+			Nom varchar2(20) not null, 
+			Prenom varchar2(20) not null, 
+			Nom_Dest varchar2(20)not null, 
+			Pays_Dest varchar2(20) not null, 
+			Nom_Hotel varchar2(20)not null,
+			Address_Hotel varchar2(50) not null,
 			Classe_Hotel number not null,
-			Prix_S float not null , Prix_D float not null,
-			Nom_circuit varchar2(20)not null, Duree_sejour number not null, 
+			Prix_S float not null , 
+			Prix_D float not null,
+			Nom_circuit varchar2(20)not null,
+			Duree_sejour number not null, 
 			Prix_Circuit float not null, 
-			Prix_Vol_Enfant float not null ,Prix_Vol_Adulte float not null, 
+			Prix_Vol_Enfant float not null ,
+			Prix_Vol_Adulte float not null, 
 			Nb_Adulte number check (Nb_Adulte>=0) , 
 			Nb_Enfant number check (Nb_Enfant >=0),
-			Description_Sejour varchar2(50) not null, Coeff_Sejour float not null,
-			Total_Vol float not null , Total_Hotel float not null, 
-			Total_Circuit float not null, Total_Facture float not null,
-			Age number, Classe_sociale varchar2(20), Dest_Pref varchar2(20), Invest_Moyen float )
+			Description_Sejour varchar2(50) not null,
+			Coeff_Sejour float not null,
+			Total_Vol float not null ,
+			Total_Hotel float not null, 
+			Total_Circuit float not null, 
+			Total_Facture float not null,
+			Age number,
+			Classe_sociale varchar2(20),
+			Dest_Pref varchar2(20), 
+			Invest_Moyen float )
 
-
---storage (initial 10M next 10M)
 partition by range(Date_Facture)
 (
 	  
@@ -228,10 +264,9 @@ INSERT INTO Assoc_Dest_Circuit VALUES (2,1);
 
 -- Remplissage de la table Séjour --
 
-INSERT INTO Sejour VALUES (1,10,'Cool',1) ;
-INSERT INTO Sejour VALUES (2,10,'Funny',1) ;
-INSERT INTO Sejour VALUES (3,21,'Chiant',0.5) ;
-INSERT INTO Sejour VALUES (4,21,'Fatiguant',0.4) ;
+INSERT INTO Sejour VALUES (1,10,'Courte durée',1.5) ;
+INSERT INTO Sejour VALUES (2,10,'Longue durée',1.0) ;
+
 
 -- Remplissage de la table Assoc_Prix_Séjour_Circuit --
 
@@ -284,8 +319,7 @@ create table LOG (
   madate timestamp,
   utilsateur varchar2(50),
   action varchar2(20),
-  cible varchar2(50) )
-;
+  cible varchar2(50) )tablespace ts11;
 
 	
 
