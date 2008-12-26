299862d040ba27ce3c1e07bbddf329fe50d729b3
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-26 13:21:02 +0000

    Alors ajout de checkbox sur les listes pour permettre la suppression.
    Reste pour les checkbox :
    - ajouter le bouton d'envoi (pourri)
    - ecrire le traitement (moins pourri)

--- projetBDA/bda_site/pages/functions.php
+++ projetBDA/bda_site/pages/functions.php
@@ -26,22 +26,29 @@ function disconnect_db($link){
 }
 
 function list_table($table_name, $link){
+  $i=0;
+
+  echo "
+<form action=\"pages/traitement_supp.php\" method=\"post\">\n";
 
   echo "  
 <center>
-  <table border=1>
-";
+  <table border=1>";
 
   $query = "SELECT column_name FROM user_tab_cols WHERE table_name='".$table_name."'";
   $stmt = ociparse($link, $query);
   ociexecute($stmt,OCI_DEFAULT);
 
-  echo "<tr>";
+  echo "
+    <tr>";
   while(OCIFetchInto ($stmt, $row, OCI_NUM)){
     foreach($row as $tmp)
-      echo "<td>".$tmp."</td>";     
+      echo "
+      <td>".$tmp."</td>";     
   }
-  echo "</tr>";
+  echo "
+      <td>DEL</td>
+    </tr>";
 
 
 
@@ -49,19 +56,31 @@ function list_table($table_name, $link){
   $query = "SELECT * FROM ".$table_name;
   $stmt = ociparse($link, $query);
   ociexecute($stmt,OCI_DEFAULT);
+  $id=0;
 
   while(OCIFetchInto ($stmt, $row, OCI_NUM)){
-    echo "<tr>";
+    echo "
+    <tr>";
   
-    foreach($row as $tmp)
-      echo "<td>".$tmp."</td>"; 
-  
-    echo "</tr>";
+    foreach($row as $tmp){
+      if($id==0)
+	$id=$tmp;
+      echo "
+      <td>".$tmp."</td>"; 
+    }
+    
+    echo "
+      <td><input type=\"checkbox\" name=\"".$table_name."".$i++."\" value=\"".$id."\"/></td>
+    </tr>";
+    $id=$i=0;
   }
 
   echo "
   </table>
 </center>";
+
+  echo "
+</form>";
 
 }
 
