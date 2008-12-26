712c0415ae5bde5f0e567acf833cc1f0d445a43c
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2008-12-26 12:46:54 +0000

    Génération des pages "liste" sous forme de fonctions

--- projetBDA/bda_site/pages/liste_etape.php
+++ projetBDA/bda_site/pages/liste_etape.php
@@ -2,61 +2,10 @@
 
 <?php
 
-$db = "(DESCRIPTION =
-    (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = dbserver)(PORT = 1521))
-    )
-    (CONNECT_DATA =
-      (SERVICE_NAME = mastergl)
-    )
-  )";
+include("functions.php");
 
-$login = "burno";
-$user = "burno";
-
-$nb_column=0;
-$table_name = "ETAPE";
-
-//Connexion à la base
-$connect = ocilogon($login, $user, $db) or die( "Could not connect to Oracle database!");
-
-echo "  
-<center>
-  <table border=1>
-";
-
-$query = "SELECT column_name FROM user_tab_cols WHERE table_name='".$table_name."'";
-$stmt = ociparse($connect, $query);
-ociexecute($stmt,OCI_DEFAULT);
-
-echo "<tr>";
-while(OCIFetchInto ($stmt, $row, OCI_NUM)){
-  foreach($row as $tmp)
-    echo "<td>".$tmp."</td>";     
-}
-  echo "</tr>";
-
-
-
-/* Nouvelle requete */
-$query = "select * from ".$table_name;
-$stmt = ociparse($connect, $query);
-ociexecute($stmt,OCI_DEFAULT);
-
-while(OCIFetchInto ($stmt, $row, OCI_NUM)){
-  echo "<tr>";
-  
-  foreach($row as $tmp)
-    echo "<td>".$tmp."</td>"; 
-  
-  echo "</tr>";
-}
-
-echo "
-  </table>
-</center>";
-
-//On se déconnecte du serveur
-ocilogoff($connect);
+$link = connect_db();
+list_table("ETAPE", $link);
+disconnect_db($link);
 
 ?>
