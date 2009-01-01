367e3657c318167eb4f1b64f39b705fc47e2689e
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2009-01-01 17:26:02 +0000

    bon j'ai clarifié un peu et virer les erreurs memoire. Je continue ce soir

--- trunk/projetFDS/src/sudoku.c
+++ trunk/projetFDS/src/sudoku.c
@@ -9,107 +9,110 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
+
+typedef unsigned short uint16_t;
 
 /*Affiche le tableau passé entre paramètre*/
 void affiche(int *tableau, int tailleTableau) {
-    int i;
+  int i;
 
-    for (i = 0; i <= tailleTableau; i++) {
-        printf("%i\n", tableau[i]);
-    }
+  for (i = 0; i <= tailleTableau; i++) {
+    printf("%i\n", tableau[i]);
+  }
 }
 
 /*
-void remplir(int *tableau, int tailleTableau) {
-    long i;
+  void remplir(int *tableau, int tailleTableau) {
+  long i;
 
-    for (i = 0; i < tailleTableau; i++) {
-        tableau[i] = -1;
-    }
-}
- */
+  for (i = 0; i < tailleTableau; i++) {
+  tableau[i] = -1;
+  }
+  }
+*/
 
 /*retourne le nombre de lignes du fichier passé entre paramètre*/
 int nbLignes(FILE *fp) {
-    int n = 0, c;
+  int n = 0, c;
 
-    while ((c = fgetc(fp)) != EOF) {
-        if (c == '\n') {
-            n++;
-        }
+  while ((c = fgetc(fp)) != EOF) {
+    if (c == '\n') {
+      n++;
     }
-    return n;
+  }
+  fseek(fp, 0, SEEK_SET); 
+  return n;
 }
 
 /*Parse la grille du fichier passé entre paramètre dans le tableau passé entre paramètre*/
-int parserGrille(char *filename, int *tab) {
-    int position = 0;
-    
-    FILE *fp;
-    //char data[10];
-    int i, j;
-    int caractereActuel = 0;
+int parserGrille(FILE *fp, int *tab) {
+  int position = 0;
+  int caractereActuel;
 
-    fp = fopen(filename, "r");
+  if (fp == NULL) {
+    printf("Erreur fichier en entrée!!!\n");
+    return EXIT_FAILURE;
+  }
 
-    //printf("Descripteur de fichier ouvert\n");
-
-    if (fp == NULL) {
-        printf("Erreur fichier en entrée!!!\n");
-        return EXIT_FAILURE;
-    }
-
-    //printf("fichier correct\n");
-
-    do // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
+  do // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
     {
-        caractereActuel = fgetc(fp); // On lit le caractère
-        //printf("%c", caractereActuel);
+      caractereActuel = fgetc(fp); // On lit le caractère
 
-        if ( (caractereActuel-'0') >=0 && (caractereActuel-'0') <=9 ){
+      if ( (caractereActuel-'0') >=0 && (caractereActuel-'0') <=9 ){
+	// C'est un chiffre
+	tab[position]=caractereActuel-'0';
+	printf("%c ", caractereActuel);
+	position++;
+      }
 
-            tab[position]=caractereActuel-'0';
-            printf("%c ", caractereActuel);
-            position++;
-        }
+      else if(caractereActuel=='_'){
+	// C'est un underscore
+	printf("%c ", caractereActuel);
+	tab[position]=-1;
+	position++;
+      }
 
-        else if(caractereActuel=='_'){
-            printf("%c ", caractereActuel);
-            tab[position]=-1;
-            position++;
-        }
-
-        else if(caractereActuel >='A' && caractereActuel <= 'F'){
-            printf("%c ", caractereActuel);
-            tab[position]=caractereActuel -'A'+10;
-            position++;
-        }
+      else if(caractereActuel >='A' && caractereActuel <= 'F'){
+	// C'est une lettre correspondant a un nombre hexadecimal
+	printf("%c ", caractereActuel);
+	tab[position]=caractereActuel -'A'+10;
+	position++;
+      }
 
 
     } while (caractereActuel != EOF);
 
-    printf("parsage fini position = %i \n",position);
+  printf("parsage fini position = %i \n",position);
 
-    fclose(fp);
-    return EXIT_SUCCESS;
+  fseek(fp, 0, SEEK_SET); 
+  return EXIT_SUCCESS;
 
 }
 
 int main(int argc, char *argv[]) {
-    FILE *fp = fopen("/home/eewans/grids/lettres.txt", "r");
-    int lignes = nbLignes(fp);
-    int taille = lignes * lignes - 1;
 
-    fclose(fp);
-    printf("Taille du tableau %i\n", taille);
-    int tab [taille];
+  /* Ouverture du fichier */
+  char *filename = argv[1];
+  FILE *fp = fopen(filename, "r");
 
+  int n = nbLignes(fp);
+  int d = sqrt(n);
 
-    parserGrille("/home/eewans/grids/lettres.txt", tab);
-    //remplir(tab, taille);
-    affiche(tab,taille);
+  printf("Taille du tableau : %ix%i\n", n, n);
+  //uint16_t grille[n*n];
+  int tab[n*n];
 
-    //int tab[15];
-    //remplir(tab,15);
-    //affiche(tab,15);
+  parserGrille(fp, tab);
+  //remplir(tab, taille);
+  //affiche(tab,taille);
+
+  //int tab[15];
+  //remplir(tab,15);
+  //affiche(tab,15);
+
+  
+  fclose(fp);
+
+  return EXIT_SUCCESS;
 }
