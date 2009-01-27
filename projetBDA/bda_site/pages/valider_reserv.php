f0471c698ad9863fb32335d0291aeab20213cebb
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2009-01-27 00:21:05 +0000

    functions -> rien n'a changé valider_reserv -> affichage des hotels Reste : les capacités ...
    ps : j'en ai vraiment chié...

--- projetBDA/bda_site/pages/valider_reserv.php
+++ projetBDA/bda_site/pages/valider_reserv.php
@@ -5,14 +5,28 @@
 include("functions.php");
 $link = connect_db();
 
-if($_GET['client'] != "")
-  $id_client = $_GET['client'];
-if($_GET['dest'] != "")
-  $id_dest = $_GET['dest'];
-if($_GET['circ'] != "")
-  $id_circ = $_GET['circ'];
-if($_GET['sejour'] != "")
-  $id_sejour = $_GET['sejour'];
+$id_client = $_GET['client'];
+$id_dest = $_GET['dest'];
+$id_circ = $_GET['circ'];
+$id_sejour = $_GET['sejour'];
+
+if($id_circ != ""){
+  // Recuperation du nombre d'etapes
+  $query = "
+ SELECT COUNT(*)
+ FROM ETAPE 
+ WHERE id_circuit='".$id_circ."'";
+  $stmt = ociparse($link, $query);
+  ociexecute($stmt,OCI_DEFAULT);
+  while(OCIFetchInto($stmt, $row, OCI_NUM))
+    $nb_total_etapes = $row[0];
+    
+  for($i=0 ; $i<$nb_total_etapes ; ++$i){
+    $id_etapes[$i] = $_GET['etape'.$i];
+    $id_hotels[$i] = $_GET['hotel'.$i];
+  }
+    
+}
 
 ?>
 
@@ -41,10 +55,10 @@ while(OCIFetchInto($stmt, $row, OCI_NUM)){
   $selected = "";
 }
 
-?>
-    </select><br/> 
+  echo "
+    </select><br/><br/>";
 
-<?php
+
 if($id_client != ""){
   echo "
     <select>
@@ -70,7 +84,7 @@ if($id_client != ""){
   }
   
   echo "
-    </select><br/>";
+    </select><br/><br/>";
 
   if($id_dest != ""){
     echo "
@@ -100,7 +114,7 @@ if($id_client != ""){
     }
   
     echo "
-    </select><br/>";
+    </select><br/><br/>";
     
   }
   
@@ -132,44 +146,116 @@ if($id_client != ""){
     }
   
     echo "
-    </select><br/>";
+    </select><br/><br/>";
     
   }
 
-//   if($id_sejour != ""){
-//     echo "
-//     <select multiple>
-//       <option value=\"\">S&eacute;lectionner une(des) &eacute;tape(s)</option>";
+  if($id_sejour != ""){
+    echo "
+    <table border=0 align=\"center\" bgcolor=\"white\">
+      <tr>
+        <td>S&eacute;lectionner une(des) &eacute;tape(s)</td>
+      </tr>";
+    $adress = $adress."&sejour=".$id_sejour;
 
-//     $adress = $adress."&sejour=".$id_sejour;
+    // Recuperation des etapes
+    $query = "
+ SELECT id_etape, no_etape, descriptif
+ FROM ETAPE 
+ WHERE id_circuit='".$id_circ."'";
+    $stmt = ociparse($link, $query);
+    ociexecute($stmt,OCI_DEFAULT);
 
-//     // Recuperation des etapes
-//     $query = "
-//  SELECT id_etape, no_etape, descriptif
-//  FROM ETAPE 
-//  WHERE id_circuit='".$id_circ."'";
-//     $stmt = ociparse($link, $query);
-//     ociexecute($stmt,OCI_DEFAULT);
+    $num_etapes=0;
 
-//     $nb_etapes=0;
+    while(OCIFetchInto($stmt, $row, OCI_NUM)){
+      $ad = $adress;
+      $ad2 = $adress;
+      for($i=0 ; $i<$nb_total_etapes ; ++$i){
+	if($num_etapes == $i){
+	  if($id_etapes[$i] != ""){
+	    $ad = $ad."&etape".$i."=";
+	    $ad2 = $ad2."&etape".$i."=".$row[0];
+	    $selected = " checked=true";
+	  }else{
+	    $ad = $ad."&etape".$i."=".$row[0];
+	    $ad2 = $ad2."&etape".$i."=";
+	  }
+	}else{
+	  $ad = $ad."&etape".$i."=".$id_etapes[$i];
+	  $ad2 = $ad2."&etape".$i."=".$id_etapes[$i];
+	}
+      }
 
-//     while(OCIFetchInto($stmt, $row, OCI_NUM)){
-//       if($row[0] == $id_etape)
-// 	$selected = "SELECTED";
-//       $ad = $adress."&etape=".$row[0];
-//       echo "
-//       <option onClick='parent.location=\"".$ad."\"'
-//               etape=
-//               value=\"".$row[0]."\"".$selected.">
-//         ".$row[0].". ".$row[1]." (".$row[2].")
-//       </option>";
-//       $selected = "";
-//     }
+      for($i=0 ; $i<$nb_total_etapes ; ++$i){
+	if($num_etapes != $i)
+	  $ad2 = $ad2."&hotel".$i."=".$id_hotels[$i];
+	$ad = $ad."&hotel".$i."=".$id_hotels[$i];
+      }      
+      
+      echo "
+      <tr>
+        <td>
+          <input type=\"checkbox\"
+                 name=\"etape".$num_etapes."\"
+                 onClick='parent.location=\"".$ad."\"'
+                 value=\"".$row[0]."\"".$selected."/>
+          ".$row[0].". ".$row[1]." (".$row[2].")
+        </td>";
+      
+      $selected = "";
+
+      // Si l'etape est choisie, on propose les hotels
+      if($id_etapes[$num_etapes] != ""){
+	// Recuperation des hotels
+	$query2 = "
+         SELECT id_hotel, nom_hotel
+         FROM HOTEL
+         WHERE id_etape='".$id_etapes[$num_etapes]."'";
+	$stmt2 = ociparse($link, $query2);
+	ociexecute($stmt2,OCI_DEFAULT);
+	
+	echo "
+        <td>
+          <select>
+            <option value=\"\">S&eacute;lectionner un h&ocirc;tel</option>";
+	
+	while(OCIFetchInto($stmt2, $row, OCI_NUM)){
+	  $ad2 = $ad2."&hotel".$num_etapes."=".$row[0];
+	  if($id_hotels[$num_etapes] == $row[0])
+	    $selected = " SELECTED";
+	  echo "
+            <option onClick='parent.location=\"".$ad2."\"'
+                    value=\"".$row[0]."\"".$selected."/>
+            ".$row[0].". H&ocirc;tel ".$row[1]."
+            </option>";
+	}
+	echo "
+          </select>
+        </td>
+        <td>
+          <select>
+            <option value=\"\">Nombre de chambre simple</option>
+          </select>
+        </td>
+        <td>
+          <select>
+            <option value=\"\">Nombre de chambre double</option>
+          </select>
+        </td>";
+	
+
+      }
+      echo "
+      </tr>";
+      $selected = "";
+      ++$num_etapes;
+    }
   
-//     echo "
-//     </select><br/>";
+    echo "
+    </table><br/>";
     
-//   }
+  }
 }
 ?>
   </form>
