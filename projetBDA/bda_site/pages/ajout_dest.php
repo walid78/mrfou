842a6171d24f507b993b24cf603c4d1de9ab0fdb
misterfou <misterfou@1a9f514f-9d4a-0410-bb98-35000676b300>
2009-02-02 16:00:34 +0000

    maj TODO affichage des tables OK. ajout et suppression OK avec redirection.

--- projetBDA/bda_site/pages/ajout_dest.php
+++ projetBDA/bda_site/pages/ajout_dest.php
@@ -1,7 +1,7 @@
 <h1>Ajouter une destination</h1>
 
 <center>
-  <form action="pages/traitement_ajout.php?table_name=DESTINATION" method="post">
+  <form action="pages/traitement_ajout.php?page=liste_dest&table_name=DESTINATION" method="post">
     <input type="text" title="NOM_DESTINATION" name="col0" value="NOM_DESTINATION"/><br/>
     <input type="text" title="PAYS" name="col1" value="PAYS"/><br/>
     <input type="submit" value="Ajouter"/>
--- projetBDA/bda_site/pages/ajout_sejour.php
+++ projetBDA/bda_site/pages/ajout_sejour.php
@@ -1,7 +1,7 @@
 <h1>Ajouter un s&eacute;jour</h1>
 
 <center>
-  <form action="pages/traitement_ajout.php?table_name=SEJOUR" method="post">
+  <form action="pages/traitement_ajout.php?page=liste_sejour&table_name=SEJOUR" method="post">
     <input type="text" title="DUREE" name="col0" value="DUREE"/><br/>
     <input type="text" title="DESCRIPTION" name="col1" value="DESCRIPTION"/><br/>
     <input type="text" title="COEFF" name="col2" value="COEFF"/><br/>
